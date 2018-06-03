ae123a65c8dfe8c0b5faf94d91f3885ad06cb785
Swoolf88 <39400654+Swoolf88@users.noreply.github.com>
2018-06-03 17:22:00 +0200

    Add files via upload

---
+++ Scripts/AeroplaneUserControl2Axis3.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Aeroplane
+{
+    [RequireComponent(typeof(AeroplaneController))]
+    public class AeroplaneUserControl2Axis3 : MonoBehaviour
+    {
+        // these max angles are only used on mobile, due to the way pitch and roll input are handled
+        public float maxRollAngle = 80;
+        public float maxPitchAngle = 80;
+        public int score1;
+        public Text m_MyText;
+        public bool hit = true;
+
+        // reference to the aeroplane that we're controlling
+        private AeroplaneController m_Aeroplane;
+
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Point" && hit == true)
+            {
+
+                score1 = score1 + 100;
+                hit = false;
+                m_MyText.text = score1.ToString();
+            }
+        }
+
+        void OnTriggerExit(Collider col)
+        {
+            if (col.gameObject.tag == "Point" && hit == false)
+            {
+
+                hit = true;
+            }
+        }
+
+        private void Awake()
+        {
+            // Set up the reference to the aeroplane controller.
+            m_Aeroplane = GetComponent<AeroplaneController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read input for the pitch, yaw, roll and throttle of the aeroplane.
+            float roll = -CrossPlatformInputManager.GetAxis("Horizontal3");
+            float pitch = -CrossPlatformInputManager.GetAxis("Vertical3");
+            bool airBrakes = CrossPlatformInputManager.GetButton("Jump3");
+
+            // auto throttle up, or down if braking.
+            float throttle = airBrakes ? -1 : 1;
+#if MOBILE_INPUT
+            AdjustInputForMobileControls(ref roll, ref pitch, ref throttle);
+#endif
+            // Pass the input to the aeroplane
+            m_Aeroplane.Move(roll, pitch, 0, throttle, airBrakes);
+        }
+
+
+        private void AdjustInputForMobileControls(ref float roll, ref float pitch, ref float throttle)
+        {
+            // because mobile tilt is used for roll and pitch, we help out by
+            // assuming that a centered level device means the user
+            // wants to fly straight and level!
+
+            // this means on mobile, the input represents the *desired* roll angle of the aeroplane,
+            // and the roll input is calculated to achieve that.
+            // whereas on non-mobile, the input directly controls the roll of the aeroplane.
+
+            float intendedRollAngle = roll * maxRollAngle * Mathf.Deg2Rad;
+            float intendedPitchAngle = pitch * maxPitchAngle * Mathf.Deg2Rad;
+            roll = Mathf.Clamp((intendedRollAngle - m_Aeroplane.RollAngle), -1, 1);
+            pitch = Mathf.Clamp((intendedPitchAngle - m_Aeroplane.PitchAngle), -1, 1);
+
+            // similarly, the throttle axis input is considered to be the desired absolute value, not a relative change to current throttle.
+            float intendedThrottle = throttle * 0.5f + 0.5f;
+            throttle = Mathf.Clamp(intendedThrottle - m_Aeroplane.Throttle, -1, 1);
+        }
+    }
+}
---
+++ Scripts/CarUserControl3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarUserControl3 : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+            float h = -CrossPlatformInputManager.GetAxis("Horizontal3");
+            float v = -CrossPlatformInputManager.GetAxis("Vertical3");
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump3");
+            m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+        }
+    }
+}
